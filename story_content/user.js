96d59af26d0a8782bd90ae7528e92d231d88ffe4
cognitonow <65226906+cognitonow@users.noreply.github.com>
2020-07-20 01:29:00 +0200

    qwe
    qwe

--- story_content/user.js
+++ story_content/user.js
@@ -2,14 +2,8 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "64Q6i1mG1A6":
+      case "61S3G7ZPWp7":
         Script1();
-        break;
-      case "5j2iRiPWPSM":
-        Script2();
-        break;
-      case "62Oxy52i6ZR":
-        Script3();
         break;
   }
 }
@@ -17,34 +11,5 @@ function ExecuteScript(strId)
 function Script1()
 {
   window.print();
-}
-
-function Script2()
-{
-  var currentDate = new Date()
-var day = currentDate.getDate()
-var month = currentDate.getMonth() + 1
-var year = currentDate.getFullYear();
-var player = GetPlayer();
-var newName = month + "/" + day + "/" +year
-player.SetVar("DateValue", newName);
-}
-
-function Script3()
-{
-  var player = GetPlayer();
-
-var theName =(
-player.GetVar("NameField")
-)
-
-var theDate =(
-player.GetVar("DateValue")
-)
-
-var urlstring = ("printCertificate.html?print=" + theName + "&" + theDate);
-
-window.open(urlstring,"_blank","width=800,height=600,menubar=no");
-
 }
 
